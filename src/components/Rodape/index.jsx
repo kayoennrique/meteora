92626b61fb8b12ae80406368f40461be8e4a1283
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-12-21 11:29:39 -0300

    Init

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+        2023 <i className="bi bi-c-circle"></i> Desenvolvido por Kayo Ennrique | Projeto
+        fictício sem fins comerciais.
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
